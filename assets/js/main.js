ccfb7a4f8175cfd44f0830bfe2cd2c8c11794748
johan <harryjohn026@gmail.com>
2021-07-02 14:33:45 +0530

    skills section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,22 @@
           $('nav').addClass('navbar-light'); 
         } 
         
+
+
+
+
+
+
+
+
+
+        $('.skills-content').waypoint(function() {
+          $('.progress .progress-bar').each(function() {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+          });
+        }, {
+          offset: '80%'
+        });
         
       });  
 })(jQuery);
