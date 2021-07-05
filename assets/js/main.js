cdd13d034f072a9dad5bb369e75ec15b0a3bb401
johan <harryjohn026@gmail.com>
2021-07-05 20:42:07 +0530

    skill animation not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,32 @@
-!(function($){
+!(function($) {
     "use strict";
-    
+
+    if ($('.typed').length) {
+        var typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
     $(window).scroll(function(e) {
-      e.preventDefault();
+        e.preventDefault();
         if ($(this).scrollTop() > 550) {
-                  $('nav').removeClass('navbar-light'); 
-                  $('nav').addClass('navbar-sky'); 
-        }else{
-          $('nav').removeClass('navbar-sky'); 
-          $('nav').addClass('navbar-light'); 
-        } 
-        
+            $('nav').removeClass('navbar-light');
+            $('.navbar-toggler i').addClass('icofont-navigation-menu1');
+            $('nav').addClass('navbar-sky');
+
+
+
+        } else {
+
+            $('.navbar-toggler i').removeClass('icofont-navigation-menu1');
+            $('nav').removeClass('navbar-sky');
+            $('nav').addClass('navbar-light');
+        }
 
 
 
@@ -21,12 +37,27 @@
 
 
         $('.skills-content').waypoint(function() {
-          $('.progress .progress-bar').each(function() {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-          });
+            $('.progress .progress-bar').each(function() {
+                $(this).css("width", $(this).attr("aria-valuenow") + '%');
+            });
         }, {
-          offset: '80%'
+            offset: '80%'
         });
-        
-      });  
+
+    });
+
+
+
+    // Init AOS
+    function aos_init() {
+        AOS.init({
+            duration: 1000,
+            easing: "ease-in-out-back",
+            once: true
+        });
+    }
+    $(window).on('load', function() {
+        aos_init();
+    });
+
 })(jQuery);
